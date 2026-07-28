ad96147b8bc8dcc465f1c7316712944f8b94f55a
EllandeVED <ellande.dascon@gmail.com>
2026-07-28 16:36:01 +0200

    Initial Commit

---
+++ NumWorks/NumWorksApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumWorksApp.swift
+//  NumWorks
+//
+//  Created by van Egmond Dascon on 28/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct NumWorksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NumWorksUITests/NumWorksUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  NumWorksUITestsLaunchTests.swift
+//  NumWorksUITests
+//
+//  Created by van Egmond Dascon on 28/07/2026.
+//
+
+import XCTest
+
+final class NumWorksUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
